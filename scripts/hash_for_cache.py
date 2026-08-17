d1b17de86022447ec52969f45022a0258092fa20
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2026-08-17 13:23:00 -0400

    Changed json encoding to work on singular proteins rather than entire lists at a time

--- scripts/hash_for_cache.py
+++ scripts/hash_for_cache.py
@@ -1,14 +1,14 @@
 from json import dumps
 from hashlib import md5
 
-# Here are defined two functions to convert a protein list and given parameters into hashes to be used to caching
+# Here are defined two functions to convert a protein and given parameter(s) into hashes to be used to caching
 
-def voxel_json_encoder(protein_list:list(str),grid_size:int):
+def voxel_json_encoder(protein:str,grid_size:int):
         # Create dictionary item, then turn into bytecode to be hashed
-        dict_item = {'protein_list':sorted(protein_list),'graph_size':grid_size}
+        dict_item = {'protein':protein,'graph_size':grid_size}
         return md5(dumps(dict_item).encode('utf-8'))
     
-def graph_json_encoder(protein_list:list(str),distance_threshold:float):
+def graph_json_encoder(protein:str,distance_threshold:float):
         # Create dictionary item, then turn into bytecode to be hashed
-        dict_item = {'protein_list':sorted(protein_list),'distance_threshold':distance_threshold}
+        dict_item = {'protein':protein,'distance_threshold':distance_threshold}
         return md5(dumps(dict_item).encode('utf-8'))
